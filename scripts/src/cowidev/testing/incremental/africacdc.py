f2b1c65b93069729c3ff2a265ac477b57463fd41
lucasrodes <lucasrodes@users.noreply.github.com>
2022-01-12 18:55:12 +0100

    refactor(testing,africacdc): working prototype using vax class

--- scripts/src/cowidev/testing/incremental/africacdc.py
+++ scripts/src/cowidev/testing/incremental/africacdc.py
@@ -1,45 +1,83 @@
-from datetime import datetime
-
 import pandas as pd
-from cowidev.utils.web import request_json
+
+from cowidev.vax.incremental.africacdc import AfricaCDC as AfricaCDCVax
+from cowidev.testing.utils.incremental import increment
 
 
-class AfricaCDC:
-    def __init__(self) -> None:
-        self._base_url = (
-            "https://services8.arcgis.com/vWozsma9VzGndzx7/ArcGIS/rest/services/"
-            "DailyCOVIDDashboard_5July21_1/FeatureServer/0/"
+ACDC_COUNTRIES = {
+    "Angola": {"name": "Angola", "notes": ""},
+    "Botswana": {
+        "name": "Botswana",
+    },
+    "Burundi": {
+        "name": "Burundi",
+    },
+    "Burkina Faso": {
+        "name": "Burkina Faso",
+    },
+    "Central African Republic": {
+        "name": "Central African Republic",
+    },
+    "Chad": {
+        "name": "Chad",
+    },
+}
+country_mapping = {country: metadata["name"] for country, metadata in ACDC_COUNTRIES.items()}
+
+
+class AfricaCDC(AfricaCDCVax):
+    _base_url = (
+        "https://services8.arcgis.com/vWozsma9VzGndzx7/ArcGIS/rest/services/"
+        "DailyCOVIDDashboard_5July21_1/FeatureServer/0/"
+    )
+    source_label = "Africa Centres for Disease Control and Prevention"
+    columns_use = [
+        "Country",
+        "Tests_Conducted",
+        "Date",
+    ]
+    columns_rename = {
+        "Country": "location",
+        "Tests_Conducted": "Cumulative total",
+        "Date": "date",
+    }
+    units = "tests performed"
+
+    def pipe_metadata(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.assign(
+            source_label=self.source_label,
+            source_url=self.source_url_ref,
         )
 
-        self.source_url_ref = "https://africacdc.org/covid-19-vaccination/"
-
-    @property
-    def source_url(self):
-        return f"{self.base_url}/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=Country%2CTests_Conducted%2CDate&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
-
-    @property
-    def source_url_date(self):
-        return f"{self._base_url}?f=pjson"
-
-    def read(self) -> pd.DataFrame:
-        data = request_json(self.source_url)
-        res = [d["attributes"] for d in data["features"]]
-
-        df = pd.DataFrame(
-            res,
-            columns=[
-                "Country",
-                "Tests_Conducted",
-                "Date",
-            ],
+    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df.pipe(self.pipe_filter_columns)
+            .pipe(self.pipe_rename)
+            .pipe(self.pipe_filter_countries, country_mapping)
+            .pipe(self.pipe_date)
+            .pipe(self.pipe_metadata)
         )
-        return df
 
-    def pipe_rename(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df.rename(
-            columns={
-                "Country": "location",
-                "Tests_Conducted": "Cumulative total",
-                "Date": "Date",
-            }
-        )
+    def increment_countries(self, df: pd.DataFrame):
+        for row in df.sort_values("location").iterrows():
+            row = row[1]
+            country = row["location"]
+            notes = ACDC_COUNTRIES[country].get("notes", "")
+            increment(
+                count=row["Cumulative total"],
+                sheet_name=country,
+                country=country,
+                units=self.units,
+                date=row["date"],
+                source_url=self.source_url,
+                source_label=self.source_label,
+                notes=notes,
+            )
+
+    def export(self):
+        df = self.read().pipe(self.pipeline)
+        self.increment_countries(df)
+
+
+def main():
+    AfricaCDC().export()
