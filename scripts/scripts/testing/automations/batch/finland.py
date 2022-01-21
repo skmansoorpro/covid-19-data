5f70d7a2222ccf8eadb6f6d939843479fff03263
Edouard Mathieu <edmat@pm.me>
2022-01-21 08:50:19 +0100

    style(testing,finland): Python formatting

--- scripts/scripts/testing/automations/batch/finland.py
+++ scripts/scripts/testing/automations/batch/finland.py
@@ -13,22 +13,27 @@ class Finland(CountryTestBase):
 
     def read(self) -> pd.DataFrame:
         df = pd.read_csv(source_url, delimiter=";")
-        df['Time'] = pd.to_datetime(df['Time'], format="%Y-%m-%d")
+        df["Time"] = pd.to_datetime(df["Time"], format="%Y-%m-%d")
         return df
 
     def pipe_metrics(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = df.pivot(index='Time', columns='Measure', values='val').fillna(0).reset_index()
+        df = df.pivot(index="Time", columns="Measure", values="val").fillna(0).reset_index()
         df = df.rename(
-            columns={"Time": "Date", "Number of cases": "positive",
-                     "Number of tests": "Daily change in cumulative total"})
+            columns={
+                "Time": "Date",
+                "Number of cases": "positive",
+                "Number of tests": "Daily change in cumulative total",
+            }
+        )
         df = df.drop("Number of deaths", axis=1)
 
-        df["Daily change in cumulative total"] = pd.to_numeric(df["Daily change in cumulative total"],
-                                                               downcast='integer')
+        df["Daily change in cumulative total"] = pd.to_numeric(
+            df["Daily change in cumulative total"], downcast="integer"
+        )
         df = df[df["Daily change in cumulative total"] != 0]
 
         df["Positive rate"] = (
-                df["positive"].rolling(7).sum() / df["Daily change in cumulative total"].rolling(7).sum()
+            df["positive"].rolling(7).sum() / df["Daily change in cumulative total"].rolling(7).sum()
         ).round(3)
 
         df["Positive rate"] = df["Positive rate"].fillna(0)
