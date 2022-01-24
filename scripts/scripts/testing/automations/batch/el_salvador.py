2e402436e6e00a1f085399519477b2b930d11128
Cam Appel <cameron@ourworldindata.org>
2022-01-24 12:49:22 +0000

    fix(el salvador): bugs

--- scripts/scripts/testing/automations/batch/el_salvador.py
+++ scripts/scripts/testing/automations/batch/el_salvador.py
@@ -13,27 +13,40 @@ class ElSalvador(CountryTestBase):
 
     def get_date(self, d):
         print(d)
-        query_url = f"{source_ulr}/?_token={self.token}&fechaMostrar={d}"
+        query_url = f"{self.source_url}/?_token={self.token}&fechaMostrar={d}"
         response = get_response(query_url)
         content = response.content
-        data = \
-            content.split("['Casos nuevos' , 'Pruebas', 'Recuperados', 'Fallecidos'],")[1].split('[],')[
-                0].strip().split("[")[1].split("]")[0].split(",")
+        data = (
+            content.decode()
+            .split("['Casos nuevos' , 'Pruebas', 'Recuperados', 'Fallecidos'],")[1]
+            .split("[],")[0]
+            .strip()
+            .split("[")[1]
+            .split("]")[0]
+            .split(",")
+        )
         data_dict = {"Date": d, "positive": data[0], "test": data[1]}
         return data_dict
 
     def read(self):
-        response = get_response(source_url)
+        response = get_response(self.source_url)
         content = response.content
-        self.token = content.split('name="_token" value="')[1].split('">')[0]
-        dates = [s.strip() for s in
-                 content.split('var datesEnabled = [')[1].split('];')[0].replace("'", "").replace("\n", "").split(",")
-                 if len(s.strip()) > 0]
+        self.token = content.decode().split('name="_token" value="')[1].split('">')[0]
+        dates = [
+            s.strip()
+            for s in content.decode()
+            .split("var datesEnabled = [")[1]
+            .split("];")[0]
+            .replace("'", "")
+            .replace("\n", "")
+            .split(",")
+            if len(s.strip()) > 0
+        ]
         result = map(self.get_date, dates)
         return pd.DataFrame.from_records(result)
 
     def pipe_metrics(self, df: pd.DataFrame) -> pd.DataFrame:
-        df['Date'] = pd.to_datetime(df['Date'], format="%d-%m-%Y")
+        df["Date"] = pd.to_datetime(df["Date"], format="%d-%m-%Y")
 
         df["Daily change in cumulative total"] = df["test"]
         df["Daily change in cumulative total"] = df["Daily change in cumulative total"].astype(int)
@@ -41,7 +54,7 @@ class ElSalvador(CountryTestBase):
         df = df[df["Daily change in cumulative total"] != 0]
 
         df["Positive rate"] = (
-                df["positive"].rolling(7).sum() / df["Daily change in cumulative total"].rolling(7).sum()
+            df["positive"].rolling(7).sum() / df["Daily change in cumulative total"].rolling(7).sum()
         ).round(3)
 
         df["Positive rate"] = df["Positive rate"].fillna(0)
