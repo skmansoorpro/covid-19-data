3c1199f58b7e253e269029b7f533ce5978383672
Rouein <rouein.fani@gmail.com>
2022-01-26 20:05:23 +0300

    (test, Sweden) script changed to calculate daily average

--- scripts/scripts/testing/automations/incremental/sweden.py
+++ scripts/scripts/testing/automations/incremental/sweden.py
@@ -1,37 +1,37 @@
-import os
 import re
+from typing import Iterator
 
 from epiweeks import Week
 from bs4 import BeautifulSoup, element
 import pandas as pd
 
-from cowidev.utils import paths
+
 from cowidev.utils.web import get_soup
-from cowidev.utils.clean import clean_count, clean_date
-from cowidev.testing.utils.incremental import increment
+from cowidev.utils.clean import clean_date, clean_count
+from cowidev.testing import CountryTestBase
 
 
-class Sweden:
+class Sweden(CountryTestBase):
     location = "Sweden"
     units = "tests performed"
     source_label = "Swedish Public Health Agency"
     notes = ""
-    source_url = "https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/statistik-och-analyser/antalet-testade-for-covid-19/"
+    source_url_ref = "https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/statistik-och-analyser/antalet-testade-for-covid-19/"
     week_num = None
     regex = {
         "title": r"Antalet testade individer och genomförda test per",
         "week": r"[vV]ecka (\d+)",
     }
 
-    def read(self) -> pd.Series:
+    def read(self) -> pd.DataFrame:
         """Reads data from source."""
 
-        soup = get_soup(self.source_url)
+        soup = get_soup(self.source_url_ref)
         data = self._parse_data(soup)
 
-        return pd.Series(data)
+        return pd.DataFrame(data)
 
-    def _parse_data(self, soup: BeautifulSoup) -> dict:
+    def _parse_data(self, soup: BeautifulSoup) -> list:
         """Gets data from the source page."""
         # Extract the relevant element
         elem = self._get_relevant_element(soup)
@@ -44,21 +44,23 @@ class Sweden:
         # Extract the text from the element
         text = self._get_text_from_element(elem)
         # Extract the metrics
-        daily_change = self._parse_metrics(text)
+        weekly_change = self._parse_metrics(text)
         # Parse date
-        date = self._parse_date()
-        # Calculate the cumulative total
-        # count = self._calc_cumulative_total(daily_change, date)   #if needed
+        week = self._parse_date()
+        # Create list
+        data = []
+        for day in week:
+            daily_change = round(weekly_change / 7.0)
+            record = {
+                "Date": clean_date(day),
+                "Daily change in cumulative total": daily_change,
+            }
+            data.append(record)
 
-        record = {
-            "source_url": self.source_url,
-            "date": date,
-            "daily_change": daily_change,
-            # "count": count,                       #if needed
-        }
-        return record
+        return data
 
     def _get_week_num_from_element(self, elem: element.Tag) -> None:
+        """Gets week number from element."""
         self.week_num = int(re.search(self.regex["week"], elem.text).group(1))
 
     def _get_relevant_element(self, soup: BeautifulSoup) -> element.Tag:
@@ -69,43 +71,23 @@ class Sweden:
 
     def _get_text_from_element(self, elem: element.Tag) -> str:
         """Gets text from element."""
-        elem = elem.find_next_sibling("table")
-        return elem.text.replace(" ", "").replace("\n", " ")
+        text = elem.find_next_sibling("table").text.replace(" ", "").replace("\n", " ")
+        return text
 
     def _parse_metrics(self, text: str) -> int:
         """Gets metrics from the text."""
         count = re.search(fr"[vV]ecka{self.week_num} \d+ (\d+)", text).group(1)
         return clean_count(count)
 
-    def _parse_date(self) -> str:
+    def _parse_date(self) -> Iterator:
         """parses the date from the week number."""
-        date = Week(2022, self.week_num, system="iso").enddate()
-        return clean_date(date)
-
-    # IF NEEDED
-
-    # def _calc_cumulative_total(self, daily_change: int, date: str) -> int:
-    #     """Calculates the cumulative total."""
-    #     output_path = os.path.join(paths.SCRIPTS.OLD, "testing", "automated_sheets", f"{self.location}.csv")
-    #     _df = pd.read_csv(output_path).sort_values("Date")
-    #     if _df["Date"].iloc[-1] == date:
-    #         return _df["Cumulative total"].iloc[-1]
-    #     count = _df["Cumulative total"].iloc[-1] + daily_change
-    #     return clean_count(count)
+        week = Week(2022, self.week_num, system="iso").iterdates()
+        return week
 
     def export(self):
         """Exports data to csv."""
-        data = self.read()
-        increment(
-            sheet_name=self.location,
-            country=self.location,
-            units=self.units,
-            date=data["date"],
-            source_url=data["source_url"],
-            source_label=self.source_label,
-            daily_change=data["daily_change"],
-            # count=data["count"],                        #if needed
-        )
+        df = self.read().pipe(self.pipe_metadata)
+        self.export_datafile(df, attach=True)
 
 
 def main():
