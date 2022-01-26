0faeb90c465fabd03d7524829cb50f2b41cbff53
Cam Appel <cameron@ourworldindata.org>
2022-01-26 18:29:45 +0000

    fix(pipeline): positive rate corrections
    Move Brazil exclusion from generate_data to testing_data_corrections

--- scripts/scripts/testing/testing_data_corrections.R
+++ scripts/scripts/testing/testing_data_corrections.R
@@ -6,6 +6,10 @@
 collated[Country == "Austria" & Date < "2021-02-01", `Short-term positive rate` := NA]
 collated[Country == "Austria" & Date < "2021-02-01", `Short-term tests per case` := NA]
 
+# Brazil: tests added in batches, creating artificially high PRs
+collated[Country == "Brazil", `Short-term positive rate` := NA]
+collated[Country == "Brazil", `Short-term tests per case` := NA]
+
 # Ecuador: the PR should start from Sept 2020, because the case data prior to that
 # included cases confirmed without PCR tests (while our data only includes PCR tests)
 collated[Country == "Ecuador" & Date < "2020-09-14", `Short-term positive rate` := NA]
