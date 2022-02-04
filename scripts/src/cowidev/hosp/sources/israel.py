607aa44f7f286b1f27dc1c5dfd92b9b70fcab338
Edouard Mathieu <edmat@pm.me>
2022-02-04 15:36:20 +0100

    enhance(hosp,israel): reinstate official source

--- scripts/src/cowidev/hosp/sources/israel.py
+++ scripts/src/cowidev/hosp/sources/israel.py
@@ -2,40 +2,45 @@ import pandas as pd
 
 
 METADATA = {
-    "source_url": "https://raw.githubusercontent.com/yuval-harpaz/covid-19-israel-matlab/master/data/Israel/dashboard_timeseries.csv",
+    "source_url": "https://datadashboardapi.health.gov.il/api/queries/hospitalizationStatus",
     "source_url_ref": "https://datadashboard.health.gov.il/COVID-19/",
-    "source_name": "Ministry of Health, via Yuval Harpaz on GitHub",
+    "source_name": "Ministry of Health",
     "entity": "Israel",
 }
 
 
 def main():
 
-    df = pd.read_csv(
-        METADATA["source_url"],
-        usecols=["date", "new_hospitalized", "CountHospitalized", "serious_critical_new", "CountCriticalStatus"],
-    )
+    df = pd.read_json(METADATA["source_url"])[
+        ["dayDate", "newHospitalized", "countHospitalized", "countBreathCum", "countCriticalStatus"]
+    ].rename(columns={"dayDate": "date"})
 
-    df["date"] = pd.to_datetime(df.date, format="%d-%b-%Y").dt.date.astype(str)
+    df["date"] = df.date.str.slice(0, 10)
 
     df = df.sort_values("date")
 
-    df["new_hospitalized"] = df.new_hospitalized.rolling(7).sum()
-    df["serious_critical_new"] = df.serious_critical_new.rolling(7).sum()
+    df["newHospitalized"] = df.newHospitalized.rolling(7).sum()
+    df["icu_admissions"] = (df.countBreathCum - df.countBreathCum.shift()).rolling(7).sum()
 
-    df.loc[df.date < "2020-09-01", "CountCriticalStatus"] = pd.NA
-
-    df = df.melt("date", var_name="indicator").dropna(subset=["value"]).sort_values("date").head(-1)
+    df = (
+        df.drop(columns="countBreathCum")
+        .melt("date", var_name="indicator")
+        .dropna(subset=["value"])
+        .sort_values("date")
+        .head(-1)
+    )
     df["indicator"] = df.indicator.replace(
         {
-            "CountHospitalized": "Daily hospital occupancy",
-            "CountCriticalStatus": "Daily ICU occupancy",
-            "serious_critical_new": "Weekly new ICU admissions",
-            "new_hospitalized": "Weekly new hospital admissions",
+            "countHospitalized": "Daily hospital occupancy",
+            "countCriticalStatus": "Daily ICU occupancy",
+            "icu_admissions": "Weekly new ICU admissions",
+            "newHospitalized": "Weekly new hospital admissions",
         }
     )
 
     df["entity"] = METADATA["entity"]
+
+    df = df.sort_values(["date", "indicator"])
 
     return df, METADATA
 
