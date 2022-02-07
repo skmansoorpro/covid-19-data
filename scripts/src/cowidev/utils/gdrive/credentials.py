752420decbc0b729f1693c6d149999d65681ab6d
lucasrodes <lucasrodes@users.noreply.github.com>
2022-02-08 00:42:52 +0100

    refactor(secrets): path to secrets

--- scripts/src/cowidev/utils/gdrive/credentials.py
+++ scripts/src/cowidev/utils/gdrive/credentials.py
@@ -1,21 +1,9 @@
 import os
-import json
-from pyaml_env import parse_config
 
 from cowidev.utils import paths
+from cowidev.utils.params import SECRETS
 
 
-def _load_google_credentials():
-    config = parse_config(paths.CONFIG_FILE, raise_if_na=False)
-    try:
-        # path = config["global"]["credentials"]
-        with open(config["global"]["credentials"], "r") as f:
-            credentials = json.load(f)
-        return credentials["google_credentials"]
-    except:
-        raise FileNotFoundError("Credentials file not found! Check global.credentials and config file.")
-
-
-CLIENT_SECRETS_PATH = _load_google_credentials()
+CLIENT_SECRETS_PATH = SECRETS.google.client_secrets
 SETTINGS_PATH = os.path.join(paths.CONFIG_DIR, "gdrive_settings.yaml")
 CREDENTIALS_PATH = os.path.join(paths.CONFIG_DIR, "gdrive_credentials.json")
