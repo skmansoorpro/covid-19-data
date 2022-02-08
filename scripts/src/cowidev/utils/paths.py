3c79a2d5faf662e5424f4d59e0e4a0107aa5f359
lucasrodes <lucasrodes@users.noreply.github.com>
2022-02-09 00:39:42 +0100

    refactor(paths): new structure

--- scripts/src/cowidev/utils/paths.py
+++ scripts/src/cowidev/utils/paths.py
@@ -1,115 +1,13 @@
 import os
-from dotenv import load_dotenv
-from dataclasses import make_dataclass
+
+# S3 ####################################
+S3_DIR = "s3://covid-19"
+S3_INTERNAL_DIR = f"{S3_DIR}/internal"
+S3_INTERNAL_VAX_DIR = f"{S3_DIR}/vax"
+S3_VAX_ICE_DIR = f"{S3_INTERNAL_VAX_DIR}/ice"
 
 
-def _get_s3_dir():
-    _s3_dir = {"INTERNAL": "internal", "INTERNAL_VAX": "internal/vax", "VAX_ICE": "s3://covid-19/internal/vax/ice"}
-    B = make_dataclass("Bucket", _s3_dir.keys(), frozen=True)
-    return B(**_s3_dir)
-
-
-def _get_scripts_dir(project_dir: str):
-    # SCRIPTS DIRECTORY
-    _SCRIPTS_DIR = os.path.join(project_dir, "scripts")
-    _SCRIPTS_OLD_DIR = os.path.join(_SCRIPTS_DIR, "scripts")
-    _SCRIPTS_GRAPHER_DIR = os.path.join(_SCRIPTS_DIR, "grapher")
-    _SCRIPTS_INPUT_DIR = os.path.join(_SCRIPTS_DIR, "input")
-    _SCRIPTS_OUTPUT_DIR = os.path.join(_SCRIPTS_DIR, "output")
-    _SCRIPTS_OUTPUT_VAX_DIR = os.path.join(_SCRIPTS_OUTPUT_DIR, "vaccinations")
-    _SCRIPTS_OUTPUT_HOSP_DIR = os.path.join(_SCRIPTS_OUTPUT_DIR, "hospitalizations")
-    _SCRIPTS_OUTPUT_TEST_DIR = os.path.join(_SCRIPTS_OUTPUT_DIR, "testing")
-    _SCRIPTS_DOCS_DIR = os.path.join(_SCRIPTS_DIR, "docs")
-    _INPUT_FOLDER_LS = [
-        "bsg",
-        "gbd",
-        "jh",
-        "reproduction",
-        "wb",
-        "cdc",
-        "gmobility",
-        "jhu",
-        "sweden",
-        "who",
-        "ecdc",
-        "iso",
-        "owid",
-        "un",
-        "yougov",
-    ]
-
-    _scripts_dirs = {
-        "OLD": _SCRIPTS_OLD_DIR,
-        "GRAPHER": _SCRIPTS_GRAPHER_DIR,
-        "INPUT": _SCRIPTS_INPUT_DIR,
-        **{f"INPUT_{e.upper()}": os.path.join(_SCRIPTS_INPUT_DIR, e) for e in _INPUT_FOLDER_LS},
-        "OUTPUT": _SCRIPTS_OUTPUT_DIR,
-        "OUTPUT_HOSP": _SCRIPTS_OUTPUT_HOSP_DIR,
-        "OUTPUT_HOSP_MAIN": os.path.join(_SCRIPTS_OUTPUT_HOSP_DIR, "main_data"),
-        "OUTPUT_HOSP_META": os.path.join(_SCRIPTS_OUTPUT_HOSP_DIR, "metadata"),
-        "OUTPUT_VAX": _SCRIPTS_OUTPUT_VAX_DIR,
-        "OUTPUT_VAX_AGE": os.path.join(_SCRIPTS_OUTPUT_VAX_DIR, "by_age_group"),
-        "OUTPUT_VAX_MANUFACT": os.path.join(_SCRIPTS_OUTPUT_VAX_DIR, "by_manufacturer"),
-        "OUTPUT_VAX_MAIN": os.path.join(_SCRIPTS_OUTPUT_VAX_DIR, "main_data"),
-        "OUTPUT_VAX_META": os.path.join(_SCRIPTS_OUTPUT_VAX_DIR, "metadata"),
-        "OUTPUT_VAX_META_AGE": os.path.join(_SCRIPTS_OUTPUT_VAX_DIR, "metadata", "locations-age.csv"),
-        "OUTPUT_VAX_META_MANUFACT": os.path.join(_SCRIPTS_OUTPUT_VAX_DIR, "metadata", "locations-manufacturer.csv"),
-        "OUTPUT_VAX_PROPOSALS": os.path.join(_SCRIPTS_OUTPUT_VAX_DIR, "proposals"),
-        "OUTPUT_VAX_LOG": os.path.join(_SCRIPTS_OUTPUT_VAX_DIR, "log"),
-        "OUTPUT_TEST": _SCRIPTS_OUTPUT_TEST_DIR,
-        "OUTPUT_TEST_MAIN": os.path.join(_SCRIPTS_OUTPUT_TEST_DIR, "main_data"),
-        # "OUTPUT_TEST_MAIN": os.path.join(_SCRIPTS_OLD_DIR, "testing", "automated_sheets"),
-        "DOCS": _SCRIPTS_DOCS_DIR,
-        "DOCS_VAX": os.path.join(_SCRIPTS_DOCS_DIR, "vaccination"),
-        "TMP": os.path.join(_SCRIPTS_DIR, "tmp"),
-        "TMP_VAX": os.path.join(_SCRIPTS_DIR, "vaccinations.preliminary.csv"),
-        "TMP_VAX_META": os.path.join(_SCRIPTS_DIR, "metadata.preliminary.csv"),
-    }
-    _scripts_dirs = {**_scripts_dirs, "INPUT_CDC_VAX": os.path.join(_scripts_dirs["INPUT_CDC"], "vaccinations")}
-    B = make_dataclass("Bucket", _scripts_dirs.keys(), frozen=True)
-    B.__repr__ = lambda _: _SCRIPTS_DIR
-    return B(**_scripts_dirs)
-
-
-def _get_data_dir(project_dir: str):
-    _PUBLIC_DIR = os.path.join(project_dir, "public")
-    _DATA_DIR = os.path.join(_PUBLIC_DIR, "data")
-    _DATA_FOLDER_LS = [
-        "ecdc",
-        "excess_mortality",
-        "internal",
-        "jhu",
-        "latest",
-        "testing",
-        "vaccinations",
-        "variants",
-        "who",
-        "hospitalizations",
-    ]
-
-    _data_dirs = {
-        "PUBLIC": _PUBLIC_DIR,
-        **{f"{e.upper()}": os.path.join(_DATA_DIR, e) for e in _DATA_FOLDER_LS},
-    }
-
-    _data_dirs = {
-        **_data_dirs,
-        "TIMESTAMP": os.path.join(_data_dirs["INTERNAL"], "timestamp"),
-        "VAX_COUNTRY": os.path.join(_data_dirs["VACCINATIONS"], "country_data"),
-        "VAX_META_MANUFACT": os.path.join(_data_dirs["VACCINATIONS"], "locations-manufacturer.csv"),
-        "VAX_META_AGE": os.path.join(_data_dirs["VACCINATIONS"], "locations-age.csv"),
-        "VAX_META": os.path.join(_data_dirs["VACCINATIONS"], "locations.csv"),
-    }
-
-    B = make_dataclass(
-        "Bucket",
-        _data_dirs.keys(),
-        frozen=True,
-    )
-    B.__repr__ = lambda _: _DATA_DIR
-    return B(**_data_dirs)
-
-
+# PROJECT DIR ###########################
 def _get_project_dir_from_env(err: bool = False):
     project_dir = os.environ.get("OWID_COVID_PROJECT_DIR")
     if project_dir is None:  # err and
@@ -118,33 +16,6 @@ def _get_project_dir_from_env(err: bool = False):
 
 
 PROJECT_DIR = _get_project_dir_from_env()
-SCRIPTS = _get_scripts_dir(PROJECT_DIR)
-DATA = _get_data_dir(PROJECT_DIR)
-S3 = _get_s3_dir()
-
-
-def out_vax(country: str, public=False, age=False, manufacturer=False, proposal=False):
-    if not public:
-        if age:
-            return os.path.join(SCRIPTS.OUTPUT_VAX_AGE, f"{country}.csv")
-        elif manufacturer:
-            return os.path.join(SCRIPTS.OUTPUT_VAX_MANUFACT, f"{country}.csv")
-        elif proposal:
-            return os.path.join(SCRIPTS.OUTPUT_VAX_PROPOSALS, f"{country}.csv")
-        return os.path.join(SCRIPTS.OUTPUT_VAX_MAIN, f"{country}.csv")
-    else:
-        return os.path.join(DATA.VAX_COUNTRY, f"{country}.csv")
-
-
-CONFIG_FILE_NEW = os.path.join(str(SCRIPTS), "config_new.yaml")
-CONFIG_FILE = os.path.join(str(SCRIPTS), "config.yaml")
-CONFIG_DIR = os.path.join(os.path.expanduser("~"), ".config", "owid")
-
-SECRETS_FILE = os.environ.get("OWID_COVID_SECRETS", None)
-
-
-### New ################################################################################################################
-
 
 # Public ########
 PUBLIC_DIR = os.path.join(PROJECT_DIR, "public")
@@ -231,6 +102,7 @@ INTERNAL_INPUT_GMOB_DIR = os.path.join(INTERNAL_INPUT_DIR, "gmobility")
 INTERNAL_INPUT_GMOB_STD_FILE = os.path.join(INTERNAL_INPUT_GMOB_DIR, "gmobility_country_standardized.csv")
 INTERNAL_INPUT_ISO_DIR = os.path.join(INTERNAL_INPUT_DIR, "iso")
 INTERNAL_INPUT_ISO_FILE = os.path.join(INTERNAL_INPUT_ISO_DIR, "iso3166_1_alpha_3_codes.csv")
+INTERNAL_INPUT_ISO_FULL_FILE = os.path.join(INTERNAL_INPUT_ISO_DIR, "iso.csv")
 INTERNAL_INPUT_JHU_DIR = os.path.join(INTERNAL_INPUT_DIR, "jhu")
 INTERNAL_INPUT_JHU_STD_FILE = os.path.join(INTERNAL_INPUT_JHU_DIR, "jhu_country_standardized.csv")
 INTERNAL_INPUT_OWID_DIR = os.path.join(INTERNAL_INPUT_DIR, "owid")
@@ -251,3 +123,27 @@ INTERNAL_GRAPHER_DIR = os.path.join(INTERNAL_DIR, "grapher")
 ## Temporary
 INTERNAL_TMP_VAX_MAIN_FILE = os.path.join(INTERNAL_DIR, "vaccinations.preliminary.csv")
 INTERNAL_TMP_VAX_META_FILE = os.path.join(INTERNAL_DIR, "metadata.preliminary.csv")
+
+# CONFIG ################################
+CONFIG_DIR = os.path.join(os.path.expanduser("~"), ".config", "owid")
+"""Where temporary files are stored."""
+SECRETS_FILE = os.environ.get("OWID_COVID_SECRETS", None)
+"""YAML with secrets, links and credentials. Not shared publicly."""
+CONFIG_FILE = os.environ.get("OWID_COVID_CONFIG", None)
+"""YAML with pipeline & execution configuration."""
+CONFIG_FILE_NEW = os.path.join(INTERNAL_DIR, "config_new.yaml")
+
+########################################################################################################################
+
+
+def out_vax(country: str, public=False, age=False, manufacturer=False, proposal=False):
+    if not public:
+        if age:
+            return os.path.join(INTERNAL_OUTPUT_VAX_AGE_DIR, f"{country}.csv")
+        elif manufacturer:
+            return os.path.join(INTERNAL_OUTPUT_VAX_MANUFACT_DIR, f"{country}.csv")
+        elif proposal:
+            return os.path.join(INTERNAL_OUTPUT_VAX_PROPOSALS_DIR, f"{country}.csv")
+        return os.path.join(INTERNAL_OUTPUT_VAX_MAIN_DIR, f"{country}.csv")
+    else:
+        return os.path.join(DATA_VAX_COUNTRY_DIR, f"{country}.csv")
