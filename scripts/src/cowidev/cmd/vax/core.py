82c16b833d9bc426c643e7386124116cb8802049
lucasrodes <lucasrodes@users.noreply.github.com>
2022-02-09 01:43:46 +0100

    enhance(cowid): add cowid vax track

--- scripts/src/cowidev/cmd/vax/core.py
+++ scripts/src/cowidev/cmd/vax/core.py
@@ -5,6 +5,7 @@ from cowidev.cmd.vax.get import click_vax_get
 from cowidev.cmd.vax.process import click_vax_process
 from cowidev.cmd.vax.generate import click_vax_generate
 from cowidev.cmd.vax.export import click_vax_export
+from cowidev.cmd.vax.track import click_vax_track
 
 
 @click.group(name="vax", chain=True, cls=OrderedGroup)
@@ -17,3 +18,4 @@ click_vax.add_command(click_vax_get)
 click_vax.add_command(click_vax_process)
 click_vax.add_command(click_vax_generate)
 click_vax.add_command(click_vax_export)
+click_vax.add_command(click_vax_track)
