2c7aa71cc37ff972865dc7f0cd24138f044e5e0c
lucasrodes <lucasrodes@users.noreply.github.com>
2022-02-09 01:21:46 +0100

    feat(cmd,vax): export step added to cowid

--- scripts/src/cowidev/cmd/vax/core.py
+++ scripts/src/cowidev/cmd/vax/core.py
@@ -1,9 +1,10 @@
 import click
 
+from cowidev.cmd.commons.utils import OrderedGroup
 from cowidev.cmd.vax.get import click_vax_get
 from cowidev.cmd.vax.process import click_vax_process
 from cowidev.cmd.vax.generate import click_vax_generate
-from cowidev.cmd.commons.utils import OrderedGroup
+from cowidev.cmd.vax.export import click_vax_export
 
 
 @click.group(name="vax", chain=True, cls=OrderedGroup)
@@ -15,3 +16,4 @@ def click_vax():
 click_vax.add_command(click_vax_get)
 click_vax.add_command(click_vax_process)
 click_vax.add_command(click_vax_generate)
+click_vax.add_command(click_vax_export)
