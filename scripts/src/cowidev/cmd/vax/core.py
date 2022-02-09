2d28185773b77b0125fff3a54b4d040739946ea5
lucasrodes <lucasrodes@users.noreply.github.com>
2022-02-09 01:19:01 +0100

    feat(cmd,vax): generate step added to cowid

--- scripts/src/cowidev/cmd/vax/core.py
+++ scripts/src/cowidev/cmd/vax/core.py
@@ -2,9 +2,11 @@ import click
 
 from cowidev.cmd.vax.get import click_vax_get
 from cowidev.cmd.vax.process import click_vax_process
+from cowidev.cmd.vax.generate import click_vax_generate
+from cowidev.cmd.commons.utils import OrderedGroup
 
 
-@click.group(name="vax", chain=True)
+@click.group(name="vax", chain=True, cls=OrderedGroup)
 def click_vax():
     """COVID-19 Vaccination data pipeline."""
     pass
@@ -12,3 +14,4 @@ def click_vax():
 
 click_vax.add_command(click_vax_get)
 click_vax.add_command(click_vax_process)
+click_vax.add_command(click_vax_generate)
