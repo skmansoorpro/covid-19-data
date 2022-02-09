0a1462620e614b8ac36af2e114eedde31a2c7a3c
lucasrodes <lucasrodes@users.noreply.github.com>
2022-02-09 19:01:54 +0100

    enhance(cowid): rename tool

--- scripts/src/cowidev/cmd/__main__.py
+++ scripts/src/cowidev/cmd/__main__.py
@@ -3,10 +3,25 @@ from cowidev.cmd.testing import click_test
 from cowidev.cmd.vax import click_vax
 
 
-@click.group()
+@click.group(name="cowid")
 def cli():
     """COVID-19 Data pipeline tool by Our World in Data."""
     pass
+
+
+# def recursive_help(cmd, parent=None):
+#     """From https://stackoverflow.com/a/58018765/5056599"""
+#     ctx = click.core.Context(cmd, info_name=cmd.name, parent=parent)
+#     print(cmd.get_help(ctx))
+#     print()
+#     commands = getattr(cmd, "commands", {})
+#     for sub in commands.values():
+#         recursive_help(sub, ctx)
+
+
+# @cli.command()
+# def dumphelp():
+#     recursive_help(cli)
 
 
 cli.add_command(click_test)
