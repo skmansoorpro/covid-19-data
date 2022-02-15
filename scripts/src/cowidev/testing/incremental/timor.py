8a9bdfd80644977b1c6294af0c927336aae43282
Rouein <rouein.fani@gmail.com>
2022-02-15 14:20:10 +0300

    refactor(test,tls): #2409

--- scripts/src/cowidev/testing/incremental/timor.py
+++ scripts/src/cowidev/testing/incremental/timor.py
@@ -1,42 +1,71 @@
-import os
 import re
-from cowidev.testing.utils.base import CountryTestBase
 
+from bs4 import BeautifulSoup, element
 import pandas as pd
 
-from cowidev.utils import get_soup
-from cowidev.utils.clean.dates import localdatenow
+from cowidev.utils import get_soup, clean_count, clean_date
+from cowidev.testing import CountryTestBase
 
 
 class TimorLeste(CountryTestBase):
-    location = "Timor"
+    location: str = "Timor"
+    units: str = "tests performed"
+    source_label: str = "Ministry of Health"
+    source_url: str = "https://covid19.gov.tl/dashboard/"
+    source_url_ref: str = "https://covid19.gov.tl/dashboard/"
+    regex: dict = {
+        "date": r"(\w+ \d{1,2}, \d{4})",
+        "element": r"Komulativo Teste",
+    }
 
-    def export(self):
-        url = "https://covid19.gov.tl/dashboard/"
-        soup = get_soup(url)
+    def read(self) -> pd.DataFrame:
+        """Read data from source"""
+        soup = get_soup(self.source_url)
+        df = self._parse_data(soup)
+        return df
 
-        stats = soup.select("#testing .c-green .wdt-column-sum")[0].text
-        count = int("".join(re.findall("[0-9]", stats)))
-        # print(count)
-
-        date_str = localdatenow("Asia/Dili")
+    def _parse_data(self, soup: BeautifulSoup) -> pd.DataFrame:
+        """Parse data from soup"""
+        # Get the element
+        elem = soup.find(text=re.compile(self.regex["element"]))
+        if not elem:
+            raise ValueError("Element not found, please update the script")
+        # Get the metrics
+        count = self._parse_metrics(elem)
+        # Get the date from soup
+        date = self._parse_date(soup)
         df = pd.DataFrame(
             {
-                "Country": self.location,
-                "Date": [date_str],
-                "Cumulative total": count,
-                "Source URL": url,
-                "Source label": "Ministry of Health",
-                "Units": "tests performed",
-                "Notes": pd.NA,
+                "Date": [date],
+                "Cumulative total": [count],
             }
         )
+        return df
 
-        if os.path.isfile(self.output_path):
-            existing = pd.read_csv(self.output_path)
-            if count > existing["Cumulative total"].max() and date_str > existing["Date"].max():
-                df = pd.concat([df, existing]).sort_values("Date", ascending=False).drop_duplicates()
-                df.to_csv(self.output_path, index=False)
+    def _parse_metrics(self, elem: element.Tag) -> int:
+        """Parse metrics from element"""
+        text = elem.find_next_sibling().text
+        count = re.sub(r"\D", "", text)
+        return clean_count(count)
+
+    def _parse_date(self, soup: BeautifulSoup) -> str:
+        """Parse date from soup"""
+        text = re.sub(r"\s+", " ", soup.text)
+        date = re.search(r"(\w+ \d{1,2}, \d{4})", text).group(1).lower()
+        return clean_date(date, "%B %d, %Y")
+
+    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Pipeline for data processing"""
+        return (
+            df.pipe(self.pipe_metadata)
+            .pipe(self.pipe_merge_current)
+            .drop_duplicates(subset=["Cumulative total"], keep="first")
+        )
+
+    def export(self):
+        """Export data to csv"""
+        df = self.read().pipe(self.pipeline)
+        self.export_datafile(df)
 
 
 def main():
