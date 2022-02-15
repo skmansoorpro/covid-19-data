3a7eb8e3654d179591839b758c523aed9eaa7c60
lucasrodes <lucasrodes@users.noreply.github.com>
2022-02-15 23:36:30 +0100

    docs: add logo

--- scripts/docs/conf.py
+++ scripts/docs/conf.py
@@ -76,9 +76,12 @@ html_theme = "furo"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_theme_options = {
+    "sidebar_hide_name": True,
+}
 
-
+# html_static_path = ["_static"]
+html_logo = "_static/owid.png"
 # autodoc_default_options = {
 #     "members": True,
 #     # The ones below should be optional but work nicely together with
