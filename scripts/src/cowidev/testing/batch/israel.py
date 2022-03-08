5e21ca662e461de7d712414004e316427709054c
Rouein <rouein.fani@gmail.com>
2022-03-08 20:51:44 +0300

    fixes (#2542)

--- scripts/src/cowidev/testing/batch/israel.py
+++ scripts/src/cowidev/testing/batch/israel.py
@@ -7,25 +7,40 @@ from cowidev.utils import clean_date_series
 
 class Israel(CountryTestBase):
     location = "Israel"
-    source_url = "https://datadashboardapi.health.gov.il/api/queries/testsPerDate"
+    source_url = "https://datadashboardapi.health.gov.il/api/queries/testResultsPerDate"
     units = "tests performed"
     source_label = "Israel Ministry of Health"
     source_url_ref = source_url
     rename_columns = {
         "date": "Date",
-        "numResultsForVirusDiagnosis": "Daily change in cumulative total",
+        "amount": "Daily change in cumulative total",
+        "positiveAmount": "positive",
     }
 
     def read(self):
-        df = pd.read_json(self.source_url)[["date", "numResultsForVirusDiagnosis"]]
+        """Reads data from the source"""
+        df = pd.read_json(self.source_url)[["date", "amount", "positiveAmount"]]
         return df
+    
+    def pipe_pr(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Calculates the positive rate"""
+        return df.assign(
+            **{
+                "Positive rate": df.positive.rolling(7)
+                .sum()
+                .div(df["Daily change in cumulative total"].rolling(7).sum())
+                .round(3).fillna(0)
+            }
+        )
 
     def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = df.pipe(self.pipe_rename_columns).pipe(self.pipe_metadata)
+        """Pipeline for the data"""
+        df = df.pipe(self.pipe_rename_columns).pipe(self.pipe_pr).pipe(self.pipe_metadata)
         df = df.assign(Date=clean_date_series(df.Date))
         return df
 
     def export(self):
+        """Export to CSV"""
         df = self.read().pipe(self.pipeline)
         self.export_datafile(df)
 
