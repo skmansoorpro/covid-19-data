c75d6bd5d6191952c94ced16f8d3ffdc5f8d7fa2
lucasrodes <lucasrodes@users.noreply.github.com>
2022-03-08 12:31:36 +0100

    fix(vax,kaz): when no boosters available #2543

--- scripts/src/cowidev/vax/incremental/kazakhstan.py
+++ scripts/src/cowidev/vax/incremental/kazakhstan.py
@@ -4,72 +4,70 @@ from selenium.webdriver.chrome.options import Options
 
 from cowidev.utils.clean import clean_count, clean_date
 from cowidev.vax.utils.incremental import increment, enrich_data
+from cowidev.vax.utils.base import CountryVaxBase
 
 
-def read(source: str) -> pd.Series:
-    op = Options()
-    op.add_argument("--headless")
-    with webdriver.Chrome(options=op) as driver:
-        driver.get(source)
-        people_vaccinated, people_fully_vaccinated = parse_vaccinations(driver)
-        total_boosters = parse_boosters(driver)
-        date = parse_date(driver)
-    return pd.Series(
-        {
-            "people_vaccinated": people_vaccinated,
-            "people_fully_vaccinated": people_fully_vaccinated,
-            "total_boosters": total_boosters,
-            "date": date,
-        }
-    )
+class Kazakhstan(CountryVaxBase):
+    location = "Kazakhstan"
+    source_url = "https://www.coronavirus2020.kz/"
 
+    def read(self) -> pd.Series:
+        op = Options()
+        op.add_argument("--headless")
+        with webdriver.Chrome(options=op) as driver:
+            driver.get(self.source_url)
+            people_vaccinated, people_fully_vaccinated = self._parse_vaccinations(driver)
+            date = self._parse_date(driver)
+            data = {
+                "people_vaccinated": people_vaccinated,
+                "people_fully_vaccinated": people_fully_vaccinated,
+                "date": date,
+            }
+            try:
+                total_boosters = self._parse_boosters(driver)
+            except IndexError:
+                pass
+            else:
+                data["total_boosters"] = total_boosters
+            return pd.Series(data)
 
-def parse_vaccinations(driver: webdriver.Chrome) -> tuple:
-    people_vaccinated = clean_count(driver.find_element_by_id("vaccinated_1").text)
-    people_fully_vaccinated = clean_count(driver.find_element_by_id("vaccinated_2").text)
-    return people_vaccinated, people_fully_vaccinated
+    def _parse_vaccinations(self, driver: webdriver.Chrome) -> tuple:
+        people_vaccinated = clean_count(driver.find_element_by_id("vaccinated_1").text)
+        people_fully_vaccinated = clean_count(driver.find_element_by_id("vaccinated_2").text)
+        return people_vaccinated, people_fully_vaccinated
 
+    def _parse_boosters(self, driver: webdriver.Chrome) -> tuple:
+        elems = driver.find_elements_by_class_name("number_revac_info")
+        elem = [e for e in elems if "Всего" in e.find_element_by_xpath("..").text][0]
+        total_boosters = clean_count(elem.text)
+        return total_boosters
 
-def parse_boosters(driver: webdriver.Chrome) -> tuple:
-    elems = driver.find_elements_by_class_name("number_revac_info")
-    elem = [e for e in elems if "Всего" in e.find_element_by_xpath("..").text][0]
-    total_boosters = clean_count(elem.text)
-    return total_boosters
+    def _parse_date(self, driver: webdriver.Chrome) -> str:
+        elem = driver.find_element_by_class_name("tabl_vactination")
+        date_str_raw = pd.read_html(elem.get_attribute("innerHTML"))[0].iloc[-1, -1]
+        return clean_date(date_str_raw, "*данные на %d.%m.%Y")
 
+    def pipe_metadata(self, ds: pd.Series):
+        ds = enrich_data(ds, "location", self.location)
+        ds = enrich_data(ds, "source_url", self.source_url)
+        return ds
 
-def parse_date(driver: webdriver.Chrome) -> str:
-    elem = driver.find_element_by_class_name("tabl_vactination")
-    date_str_raw = pd.read_html(elem.get_attribute("innerHTML"))[0].iloc[-1, -1]
-    return clean_date(date_str_raw, "*данные на %d.%m.%Y")
+    def pipe_vaccine(self, ds: pd.Series):
+        return enrich_data(ds, "vaccine", "QazVac, Sinopharm/Beijing, Sputnik V")
 
+    def pipe_metrics(self, ds: pd.Series):
+        if "total_boosters" in ds:
+            total_vaccintations = ds["people_vaccinated"] + ds["people_fully_vaccinated"] + ds["total_boosters"]
+            return enrich_data(ds, "total_vaccinations", total_vaccintations)
+        return ds
 
-def enrich_location(ds: pd.Series):
-    return enrich_data(ds, "location", "Kazakhstan")
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        return ds.pipe(self.pipe_metadata).pipe(self.pipe_vaccine).pipe(self.pipe_metrics)
 
-
-def enrich_vaccine(ds: pd.Series):
-    return enrich_data(ds, "vaccine", "QazVac, Sinopharm/Beijing, Sputnik V")
-
-
-def add_totals(ds: pd.Series):
-    total_vaccintations = ds["people_vaccinated"] + ds["people_fully_vaccinated"] + +ds["total_boosters"]
-    return enrich_data(ds, "total_vaccinations", total_vaccintations)
-
-
-def pipeline(ds: pd.Series) -> pd.Series:
-    return ds.pipe(enrich_location).pipe(enrich_vaccine).pipe(add_totals)
+    def export(self):
+        data = self.read().pipe(self.pipeline)
+        self.export_datafile(data, attach=True)
 
 
 def main():
-    source = "https://www.coronavirus2020.kz/"
-    data = read(source).pipe(pipeline)
-    increment(
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_vaccinated=data["people_vaccinated"],
-        people_fully_vaccinated=data["people_fully_vaccinated"],
-        total_boosters=data["total_boosters"],
-        date=data["date"],
-        source_url=source,
-        vaccine=data["vaccine"],
-    )
+    Kazakhstan().export()
