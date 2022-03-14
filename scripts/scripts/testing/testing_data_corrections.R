0738052d70f0d2ab4404273794f293541201b860
Cam Appel <cameron@ourworldindata.org>
2022-03-14 15:31:11 -0400

    data(testing+xm): update

--- scripts/scripts/testing/testing_data_corrections.R
+++ scripts/scripts/testing/testing_data_corrections.R
@@ -30,3 +30,7 @@ collated[Country == "Qatar", `Short-term tests per case` := NA]
 # Fiji: estimated positive rate does not match that of the official source
 collated[Country == "Fiji", `Short-term positive rate` := NA]
 collated[Country == "Fiji", `Short-term tests per case` := NA]
+
+# Fiji: estimated positive rate does not match that of the official source
+collated[Country == "New Zealand", `Short-term positive rate` := NA]
+collated[Country == "New Zealand", `Short-term tests per case` := NA]
