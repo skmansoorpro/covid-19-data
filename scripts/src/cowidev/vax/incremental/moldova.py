363b5d520ccf5fefed69929f8d5e0d1e093db18b
lucasrodes <lucasrodes@users.noreply.github.com>
2022-03-15 22:36:53 +0100

    enhance(vax,mda): get metrics from WHO #2554

--- scripts/src/cowidev/vax/incremental/moldova.py
+++ scripts/src/cowidev/vax/incremental/moldova.py
@@ -4,59 +4,56 @@ import pandas as pd
 from cowidev.utils.clean import clean_count
 from cowidev.utils.clean.dates import localdate
 from cowidev.utils.web import get_soup
-from cowidev.vax.utils.incremental import enrich_data, increment
+from cowidev.vax.utils.incremental import enrich_data
+from cowidev.vax.utils.base import CountryVaxBase
+from cowidev.vax.utils.utils import add_latest_who_values
 
 
-def read(source: str) -> pd.Series:
-    soup = get_soup(source)
-    return parse_data(soup)
+class Moldova(CountryVaxBase):
+    location = "Moldova"
+    source_url = "https://vaccinare.gov.md"
 
+    def read(self) -> pd.Series:
+        soup = get_soup(self.source_url)
+        return self._parse_data(soup)
 
-def parse_data(soup: BeautifulSoup) -> pd.Series:
+    def _parse_data(self, soup: BeautifulSoup) -> pd.Series:
 
-    total_vaccinations = clean_count(soup.find(id="stats").find_all("span")[0].text)
-    people_fully_vaccinated = clean_count(soup.find(id="stats").find_all("span")[1].text)
+        total_vaccinations = clean_count(soup.find(id="stats").find_all("span")[0].text)
+        people_fully_vaccinated = clean_count(soup.find(id="stats").find_all("span")[1].text)
 
-    data = {
-        "total_vaccinations": total_vaccinations,
-        "people_fully_vaccinated": people_fully_vaccinated,
-    }
-    return pd.Series(data=data)
+        data = {
+            "total_vaccinations": total_vaccinations,
+            "people_fully_vaccinated": people_fully_vaccinated,
+        }
+        return pd.Series(data=data)
 
+    def format_date(self, ds: pd.Series) -> pd.Series:
+        date = localdate("Europe/Chisinau")
+        return enrich_data(ds, "date", date)
 
-def format_date(ds: pd.Series) -> pd.Series:
-    date = localdate("Europe/Chisinau")
-    return enrich_data(ds, "date", date)
+    def enrich_location(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "location", "Moldova")
 
+    def enrich_vaccine(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(
+            ds,
+            "vaccine",
+            "Johnson&Johnson, Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing, Sputnik V",
+        )
 
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "location", "Moldova")
+    def enrich_source(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "source_url", self.source_url)
 
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        ds = ds.pipe(self.format_date).pipe(self.enrich_location).pipe(self.enrich_vaccine).pipe(self.enrich_source)
+        df = add_latest_who_values(ds, "Republic of Moldova", ["people_vaccinated"])
+        return df
 
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(
-        ds,
-        "vaccine",
-        "Johnson&Johnson, Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing, Sputnik V",
-    )
-
-
-def enrich_source(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "source_url", "https://vaccinare.gov.md/")
-
-
-def pipeline(ds: pd.Series) -> pd.Series:
-    return ds.pipe(format_date).pipe(enrich_location).pipe(enrich_vaccine).pipe(enrich_source)
+    def export(self):
+        df = self.read().pipe(self.pipeline)
+        self.export_datafile(df, attach=True)
 
 
 def main():
-    source = "https://vaccinare.gov.md/"
-    data = read(source).pipe(pipeline)
-    increment(
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_fully_vaccinated=data["people_fully_vaccinated"],
-        date=data["date"],
-        source_url=data["source_url"],
-        vaccine=data["vaccine"],
-    )
+    Moldova().export()
